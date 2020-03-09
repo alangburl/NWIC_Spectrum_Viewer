26bb9a3be5744e8b9a354deef3a6b6c304bad60d
Alan <alangburl@gmail.com>
2020-03-08 22:25:57 -0500

    Working list mode reader, need to have save feature incorporated and integrated into overall gui

--- List_Mode_Reader.py
+++ List_Mode_Reader.py
@@ -2,22 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os,sys,time
 class List_Mode():
-    def __init__(self,delta_time,file_location=None,sync_file_location=None,
-                 num_channels=8192):
+    def __init__(self,num_channels=8192):
         '''Delta time: offset after the sync pulse used to define regions 1 &2
                         in micro seconds
         '''
         super().__init__()
-        self.file_location=sync_file_location
-        self.location=file_location
-        self.delta_time=delta_time
         self.num_channels=num_channels
         
     def read_file(self,file_location):
         '''Open and read the file into memory
         '''
-#        dty=np.dtype([('Time','f8'),('Heigt','f8')])
-#        lis=np.loadtxt(self.file_location,delimiter=';',usecols=(0,1),dtype=dty,skiprows=1)
         s=time.time()
         f=open(file_location,'r')
         data=f.readlines()
@@ -34,9 +28,10 @@ class List_Mode():
         print('Loaded in {:.2f} s'.format(time.time()-s))
         return timing,channel
     
-    def timing(self):
+    def timing(self,delta_time,s_time,d_time,d_channels):
+        self.delta_time=delta_time
         #get the sync pulse information
-        s_time,s_channels=self.read_file(self.file_location)
+#        s_time,s_channels=self.read_file(self.file_location)
         #create two dictionaries: 1 for region one  and the other for region 2
         self.region1_spectrum={}
         self.region2_spectrum={}
@@ -46,43 +41,41 @@ class List_Mode():
         #two different lists one to go from the sync pulse to the 
         #region divider time and the other from the region divider time
         #to the next sync pulse
-        region_1_timing_splits=np.linspace(0,self.delta_time,10)
-        region_2_timing_splits=np.linspace(self.delta_time,
-                                           s_time[1]-s_time[0],10)
-        region_1_counts=[0]*len(region_1_timing_splits)
-        region_2_counts=[0]*len(region_2_timing_splits)
+#        region_1_timing_splits=np.linspace(0,self.delta_time,10)
+#        region_2_timing_splits=np.linspace(self.delta_time,
+#                                           s_time[1]-s_time[0],10)
+#        region_1_counts=[0]*len(region_1_timing_splits)
+#        region_2_counts=[0]*len(region_2_timing_splits)
         #get the spectral data 
-        d_time,d_channels=self.read_file(self.location)
+#        d_time,d_channels=self.read_file(self.location)
         #loop through the sync pulse times
         d_loc=0
         s=time.time()
         for i in range(len(s_time)-1):
             #loop through the spectral data until the next sync pulse occurs
             while d_time[d_loc]<s_time[i]+self.delta_time:
-                for j in range(len(region_1_timing_splits)-1):
-                    if d_time[d_loc]<region_1_timing_splits[j+1]+s_time[i] \
-                    and d_time[d_loc]>=region_1_timing_splits[j]+s_time[i]:
-                        region_1_counts[j]+=1
-                        break
+#                for j in range(len(region_1_timing_splits)-1):
+#                    if d_time[d_loc]<region_1_timing_splits[j+1]+s_time[i] \
+#                    and d_time[d_loc]>=region_1_timing_splits[j]+s_time[i]:
+#                        region_1_counts[j]+=1
+#                        break
                 self.region1_spectrum[d_channels[d_loc]]+=1
                 d_loc+=1
                 
             while d_time[d_loc]<s_time[i+1]:
-                for j in range(len(region_2_timing_splits)-1):
-                    if d_time[d_loc]<region_2_timing_splits[j+1]+s_time[i] \
-                    and d_time[d_loc]>=region_2_timing_splits[j]+s_time[i]:
-                        region_2_counts[j]+=1
-                        break
+#                for j in range(len(region_2_timing_splits)-1):
+#                    if d_time[d_loc]<region_2_timing_splits[j+1]+s_time[i] \
+#                    and d_time[d_loc]>=region_2_timing_splits[j]+s_time[i]:
+#                        region_2_counts[j]+=1
+#                        break
                 self.region2_spectrum[d_channels[d_loc]]+=1
                 d_loc+=1
         print('Process time {:.2f}'.format(time.time()-s))
-        self.save_spectrum()
+#        self.save_spectrum()
         del s_time
-        del s_channels
         del d_time
         del d_channels
-        return [self.region2_spectrum,region_2_counts,self.region1_spectrum,
-                region_1_counts,region_1_timing_splits,region_2_timing_splits]
+        return [self.region1_spectrum,self.region2_spectrum]
              
     def save_spectrum(self):
         '''save the spectrum in region one and two
@@ -94,25 +87,25 @@ class List_Mode():
             h.write('{}\n'.format(self.region2_spectrum[i])) 
         f.close()
         h.close()
-if __name__=="__main__":
-        loc=os.path.join(os.getcwd(),'Test_Data')
-        file=os.path.join(loc,'1_90505.csv')
-        file1=os.path.join(loc,'3_90505.csv')
-        a=List_Mode(70,file1,file)
-        spec2,coun2,spec1,coun1,t1,t2=a.timing()
-        plt.figure(1)
-        plt.plot(list(spec2.keys())[0:2000],list(spec2.values())[0:2000])
-        plt.yscale('log')
-        plt.ylabel('Counts')
-        plt.xlabel('Channels')
-        plt.title('Region 2 Spectrum')
-        plt.savefig('Region2.png',dpi=600,figsize=(6,6))
-        plt.show()
-        plt.figure(2)
-        plt.plot(list(spec1.keys())[0:2000],list(spec1.values())[0:2000])
-        plt.yscale('log')
-        plt.ylabel('Counts')
-        plt.xlabel('Channels')
-        plt.title('Region 1 Spectrum')
-        plt.savefig('Region1.png',dpi=600,figsize=(6,6))
-        plt.show()
+#if __name__=="__main__":
+#        loc=os.path.join(os.getcwd(),'Test_Data')
+#        file=os.path.join(loc,'905_1.csv')
+#        file1=os.path.join(loc,'905_3.csv')
+#        a=List_Mode(file1,file)
+#        spec2,coun2,spec1,coun1,t1,t2=a.timing(70)
+#        plt.figure(1)
+#        plt.plot(list(spec2.keys())[0:2000],list(spec2.values())[0:2000])
+#        plt.yscale('log')
+#        plt.ylabel('Counts')
+#        plt.xlabel('Channels')
+#        plt.title('Region 2 Spectrum')
+#        plt.savefig('Region2.png',dpi=600,figsize=(6,6))
+#        plt.show()
+#        plt.figure(2)
+#        plt.plot(list(spec1.keys())[0:2000],list(spec1.values())[0:2000])
+#        plt.yscale('log')
+#        plt.ylabel('Counts')
+#        plt.xlabel('Channels')
+#        plt.title('Region 1 Spectrum')
+#        plt.savefig('Region1.png',dpi=600,figsize=(6,6))
+#        plt.show()
--- List_Mode_Viewer.py
+++ List_Mode_Viewer.py
@@ -16,6 +16,9 @@ from matplotlib.backends.backend_qt5agg import (
 
 
 class List_Mode_Viewer(QMainWindow):
+    sync=False
+    lis=False
+    calibration=False
     def __init__(self):
         super().__init__()
         self.setWindowTitle('List Mode Viewer')
@@ -23,8 +26,9 @@ class List_Mode_Viewer(QMainWindow):
         self.font.setPointSize(12)
         self.size_policy=QSizePolicy.Expanding
         self.menu()
-        
+        self.geometry()
         self.showMaximized()
+        self.popup()
         self.show()
         
     def menu(self):
@@ -32,12 +36,272 @@ class List_Mode_Viewer(QMainWindow):
         self.load_new=QAction('&Load New Data')
         self.load_new.triggered.connect(self.loading)
         self.load_new.setShortcut('CTRL+N')
-    
+        
+        self.menuView=self.menuBar().addMenu('&View')
+        self.view_pop=QAction('&Show Tools')
+        self.view_pop.triggered.connect(self.popup)
+        self.view_pop.setShortcut('CTRL+U')
+        self.menuView.addActions([self.view_pop])
         self.menuFile.addActions([self.load_new])
     def geometry(self):
+        self.central_widget=QWidget()
+        self.region1_plot=QWidget()
+        self.region1_figure=Figure()
+        self.region1_canvas=FigureCanvas(self.region1_figure)
+        self.region1_toolbar=NavigationToolbar(self.region1_canvas,self)
+        layout=QVBoxLayout()
+        layout.addWidget(self.region1_toolbar)
+        layout.addWidget(self.region1_canvas)
+        self.region1_plot.setLayout(layout)
+        self.region1_ax=self.region1_canvas.figure.subplots()
+        
+        self.region2_plot=QWidget()
+        self.region2_figure=Figure()
+        self.region2_canvas=FigureCanvas(self.region2_figure)
+        self.region2_toolbar=NavigationToolbar(self.region2_canvas,self)
+        layout=QVBoxLayout()
+        layout.addWidget(self.region2_toolbar)
+        layout.addWidget(self.region2_canvas)
+        self.region2_plot.setLayout(layout)
+        self.region2_ax=self.region2_canvas.figure.subplots()
+        self.region2_ax.set_title('Region 2')
+        
+        self.total_plot=QWidget()
+        self.total_figure=Figure()
+        self.total_canvas=FigureCanvas(self.total_figure)
+        self.total_toolbar=NavigationToolbar(self.total_canvas,self)
+        layout=QVBoxLayout()
+        layout.addWidget(self.total_toolbar)
+        layout.addWidget(self.total_canvas)
+        self.total_plot.setLayout(layout)
+        self.total_ax=self.total_canvas.figure.subplots()
+        self.total_ax.set_title('Total')
+        
+        layout=QGridLayout()
+        layout.addWidget(self.region1_plot,0,0)
+        layout.addWidget(self.region2_plot,0,1)
+        layout.addWidget(self.total_plot,1,0,1,2)
+        self.central_widget.setLayout(layout)
+        self.setCentralWidget(self.central_widget)
         
     def loading(self):
-        pass
+        self.loader=QWidget()
+        self.loader.setWindowFlags(Qt.WindowStaysOnTopHint)
+        self.sync_label=QLabel('Sync Pulse Location: ')
+        self.sync_label.setSizePolicy(self.size_policy,self.size_policy)
+        self.sync_label.setFont(self.font)
+        
+        self.sync_location=QPushButton('Browse')
+        self.sync_location.setSizePolicy(self.size_policy,self.size_policy)
+        self.sync_location.setFont(self.font)
+        self.sync_location.clicked.connect(self.sync_browse)
+        
+        self.detector_label=QLabel('Detector List Mode Data: ')
+        self.detector_label.setSizePolicy(self.size_policy,self.size_policy)
+        self.detector_label.setFont(self.font)
+        
+        self.detector_location=QPushButton('Browse')
+        self.detector_location.setSizePolicy(self.size_policy,self.size_policy)
+        self.detector_location.setFont(self.font)
+        self.detector_location.clicked.connect(self.detector_browse)
+        
+        self.calibration_label=QLabel('Calibration:(Optional)')
+        self.calibration_label.setSizePolicy(self.size_policy,self.size_policy)
+        self.calibration_label.setFont(self.font)
+        
+        self.calibration_location=QPushButton('Browse')
+        self.calibration_location.setSizePolicy(self.size_policy,self.size_policy)
+        self.calibration_location.setFont(self.font)
+        self.calibration_location.clicked.connect(self.calibration_browse)
+        
+        self.process_new=QPushButton('Process')
+        self.process_new.setSizePolicy(self.size_policy,self.size_policy)
+        self.process_new.setFont(self.font)
+        self.process_new.clicked.connect(self.processing_new)
+        self.process_new.setEnabled(False)
+        
+        layout=QGridLayout()
+        layout.addWidget(self.sync_label,0,0)
+        layout.addWidget(self.sync_location,0,1)
+        layout.addWidget(self.detector_label,1,0)
+        layout.addWidget(self.detector_location,1,1)
+        layout.addWidget(self.calibration_label,2,0)
+        layout.addWidget(self.calibration_location,2,1)
+        layout.addWidget(self.process_new,3,0,1,2)
+        self.loader.setLayout(layout)
+        self.loader.setWindowTitle('Load New List Mode')
+        self.loader.show()
+        
+    def sync_browse(self):
+        self.sync_filename=QFileDialog.getOpenFileName(self,
+                           'Sync File Location',"",
+                           'Text File (*.txt);;Comma Seperated File (*.csv)')
+        if self.sync_filename[0]!="":
+            self.sync=True
+            if self.lis==True:
+                self.process_new.setEnabled(True)
+    def detector_browse(self):
+        self.list_filename=QFileDialog.getOpenFileName(self,
+                           'Listmode File Location',"",
+                           'Text File (*.txt);;Comma Seperated File (*.csv)')
+        if self.list_filename[0]!="":
+            self.lis=True
+            if self.sync==True:
+                self.process_new.setEnabled(True)
+    def calibration_browse(self):
+        self.calib_filename=QFileDialog.getOpenFileName(self,
+                           'calibration File Location',"",
+                           'Text File (*.txt);;Comma Seperated File (*.csv)')
+        if self.calib_filename[0]!='':
+            self.calibration=True
+            f=open(self.calib_filename[0],'r')
+            data=f.readlines()
+            f.close()
+            self.calibration_data=[]
+            for i in range(len(data)):
+                self.calibration_data.append(float(data[i]))
+            del data
+            del f
+    def processing_new(self):
+        try:
+            self.loader.close()
+        except:
+            True
+        self.list_mode_processor=List_Mode()
+        self.sync_time,sync_channel=self.list_mode_processor.read_file(
+                self.sync_filename[0])
+        del sync_channel
+        self.list_time,self.list_channel=self.list_mode_processor.read_file(
+                self.list_filename[0])
+        delt=(self.sync_time[2]-self.sync_time[1])
+        self.offset.setMaximum(delt-self.duty_cycle.value()/100*delt)
+        self.updater()
+        
+    def popup(self):
+        self.popup=QWidget()
+        self.popup.setWindowTitle('View Controls')
+        self.popup.setSizePolicy(self.size_policy,self.size_policy)
+        self.popup.setFont(self.font)
+        
+        self.duty_label=QLabel('Duty Cycle [%]: ')
+        self.duty_label.setSizePolicy(self.size_policy,self.size_policy)
+        self.duty_label.setFont(self.font)
+        
+        self.duty_cycle=QSlider(Qt.Horizontal)
+        self.duty_cycle.setSizePolicy(self.size_policy,self.size_policy)
+        self.duty_cycle.setFont(self.font)
+        self.duty_cycle.setMinimum(0)
+        self.duty_cycle.setMaximum(100)
+        self.duty_cycle.setSingleStep(1)
+        self.duty_cycle.setValue(10)
+        self.duty_cycle.setTickPosition(QSlider.TicksBelow)
+        self.duty_cycle.valueChanged.connect(self.duty_changed)
+        
+        self.duty_indicator=QLineEdit()
+        self.duty_indicator.setSizePolicy(self.size_policy,self.size_policy)
+        self.duty_indicator.setFont(self.font)
+        self.duty_indicator.setToolTip(
+                'Duty cycle of the sync pulse from\neutron generator')
+        
+        self.offset_label=QLabel('Offset from sync [us]: ')
+        self.offset_label.setSizePolicy(self.size_policy,self.size_policy)
+        self.offset_label.setFont(self.font)
+        
+        self.offset=QSlider(Qt.Horizontal)
+        self.offset.setSizePolicy(self.size_policy,self.size_policy)
+        self.offset.setFont(self.font)
+        self.offset.setMinimum(0)
+        self.offset.setMaximum(1000)
+        self.offset.setSingleStep(1)
+        self.offset.setValue(10)
+        self.offset.setTickPosition(QSlider.TicksBelow)
+        self.offset.setTickInterval(100)
+        self.offset.setToolTip(
+            'After end of pulse to divide into Region 1 and 2 in micro seconds')
+#        self.offset.valueChanged.connect(self.offset_changed)
+        self.offset.valueChanged.connect(self.offset_changed)
+        self.offset_indicator=QLineEdit()
+        self.offset_indicator.setSizePolicy(self.size_policy,self.size_policy)
+        self.offset_indicator.setFont(self.font)
+        self.offset_indicator.setToolTip(
+            'After end of pulse to divide into Region 1 and 2 in micro seconds')
+        
+        self.update=QPushButton('Update')
+        self.update.setSizePolicy(self.size_policy,self.size_policy)
+        self.update.setFont(self.font)
+        self.update.clicked.connect(self.updater)
+        
+        layout=QGridLayout()
+        layout.addWidget(self.duty_label,0,0)
+        layout.addWidget(self.duty_cycle,0,1)
+        layout.addWidget(self.duty_indicator,0,2)
+        layout.addWidget(self.offset_label,1,0)
+        layout.addWidget(self.offset,1,1)
+        layout.addWidget(self.offset_indicator,1,2)
+        layout.addWidget(self.update,2,0)
+        
+        self.popup.setLayout(layout)
+        self.duty_changed()
+        self.offset_changed()
+        self.popup.show()
+    def duty_changed(self):
+        self.duty_indicator.setText(str(self.duty_cycle.value()))
+        
+    def offset_changed(self):
+        self.offset_indicator.setText(str((self.offset.value())))
+        
+    def updater(self):
+        delta_time=self.duty_cycle.value()/100*(
+                self.sync_time[2]-self.sync_time[1])
+        delta_time+=self.offset.value()
+        self.region1_spec,self.region2_spec=self.list_mode_processor.timing(
+                                        delta_time,self.sync_time,
+                                        self.list_time,self.list_channel)
+        total=[]
+        for i in range(len(list(self.region2_spec.values()))):
+            total.append(list(self.region2_spec.values())[i]+list(self.region1_spec.values())[i])
+        self.region1_ax.clear()
+        self.region2_ax.clear()
+        self.total_ax.clear()
+        self.region1_ax.set_title('Region 1')
+        self.region2_ax.set_title('Region 2')
+        self.total_ax.set_title('Total')
+        r1_values=list(self.region1_spec.values())[:-1]
+        r2_values=list(self.region2_spec.values())[:-1]
+        uncal_keys=list(self.region1_spec.keys())
+        if self.calibration:
+            self.region1_ax.set_xlim(self.calibration_data[0],
+                                     14)
+            self.region2_ax.set_xlim(self.calibration_data[0],
+                                     14)
+            self.total_ax.set_xlim(self.calibration_data[0],14)
+            self.region1_ax.set_xlabel('Energy [MeV]')
+            self.region2_ax.set_xlabel('Energy [MeV]')
+            self.total_ax.set_xlabel('Energy [MeV]')
+            self.region1_ax.plot(self.calibration_data,r1_values)
+            self.region2_ax.plot(self.calibration_data,r2_values)
+            self.total_ax.plot(self.calibration_data,total[:-1],label='Total')
+            self.total_ax.plot(self.calibration_data,r1_values,label='Region 1')
+            self.total_ax.plot(self.calibration_data,r2_values,label='Region 2')
+        else:
+            self.region1_ax.set_xlabel('Channel')
+            self.region2_ax.set_xlabel('Channel')
+            self.total_ax.set_xlabel('Channel')
+            self.region1_ax.set_xlim(0,len(list(self.region1_spec.keys())))
+            self.region2_ax.set_xlim(0,len(list(self.region2_spec.keys())))
+            self.total_ax.set_xlim(0,len(list(self.region2_spec.keys())))
+            self.region1_ax.plot(uncal_keys,r1_values)
+            self.region2_ax.plot(uncal_keys,r2_values)
+            self.total_ax.plot(uncal_keys,total[:-1],label='Total')
+            self.total_ax.plot(uncal_keys,r1_values,label='Region 1')
+            self.total_ax.plot(uncal_keys,r2_values,label='Region 2')
+        self.region1_ax.set_yscale('log')
+        self.region2_ax.set_yscale('log')
+        self.total_ax.set_yscale('log')
+        self.total_ax.legend()
+        self.region1_canvas.draw()
+        self.region2_canvas.draw()
+        self.total_canvas.draw()
         
 if __name__ =="__main__":
     app=QApplication(sys.argv)
