a51351d0f366a3b0b4bebbe96fcbe99670016f3d
Alan <alangburl@gmail.com>
2020-12-10 22:58:16 -0600

    Typo fix

--- conversion_setup.py
+++ conversion_setup.py
@@ -3,4 +3,4 @@ from Cython.Build import cythonize
 import setuptools
 setup(ext_modules=cythonize("Conversion.pyx", build_dir="build"),
                                            script_args=['build'], 
-                                           options={'build':{'build_lib':'.'}}
+                                           options={'build':{'build_lib':'.'}})
