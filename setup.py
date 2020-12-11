b581d74567c5d51a1842a76c08420f1d294a75b8
Alan <alangburl@gmail.com>
2020-12-10 22:27:00 -0600

    Add a setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+#This is the setup script. It will build the required packages for your
+#version of python. If you don't have Cython. It will download it for you
+#using pip install cython
+import sys
+import subprocess
+try:
+    import cython
+except ImportError as e:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install','cython'])
+    reqs = subprocess.check_output([sys.executable, '-m', 'pip','freeze'])
+    install=[r.decode().split('==')[0] for r in reqs.split()]
+    print('Installed: {}'.format(install))
+
+files_to_build=['covnersion_setup.py',
+                'ROI_Arrival_setup.py',
+                'timing_setup.py']
+
+for i in files_to_build:
+    subprocess.check_call(['python', i, 'build_ext', '--inplace'])
+    
